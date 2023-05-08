4b262c511c0db01bdc11010661074e4eecc990d2
franjavi01 <36012280+franjavi01@users.noreply.github.com>
2023-05-08 15:57:03 +0200

    añadidas cards de bootstrap a traves de json y el bucle foreach

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php include 'includes/header.php'; ?>
-    <p>Esto es el main</p>
+<?php 
+$miJSON = file_get_contents('FichasPeliculas.json');
+$datos = json_decode($miJSON, true);
+
+echo '<div class="container">';
+echo '<div class="row">';
+foreach($datos as $dato){
+echo '<div class="card text-center">';
+echo '<i class="'.$dato['imagen'].' card-img-top"  alt="'.$dato['titulo'].'"></i>';
+echo '<div class="card-body">';
+echo '<h3 class="card-title">'.$dato['titulo'].'</h3>';
+echo '<p class="card-text">'.$dato['texto'].'</p>';
+echo '<a href="'.$dato['enlace'].'" class="btn btn-primary">Entrar</a>';
+echo '</div>';
+echo '</div>';
+}
+echo '</div>';
+echo '</div>';
+
+
+?>
 <?php include 'includes/footer.php'; ?>
+
     
